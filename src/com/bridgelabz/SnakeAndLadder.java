0620d7a754a0f0b53af1bca7f41af30d056e6877
Prathameshvardam321 <prathameshvardam321@gmail.com>
2022-09-30 17:42:03 +0530

    Added UC3.

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -7,7 +7,26 @@ public class SnakeAndLadder {
         Random rand = new Random();
         int position1=0;
         int dieRoll = rand.nextInt(6)+1;
-        System.out.println(dieRoll);
+        System.out.println("Dieroll is : "+dieRoll);
+        position1+=dieRoll;
+        System.out.println("Player 1 : "+position1);
+        int option = rand.nextInt(3);
+       switch (option){
+           case 0 :
+               System.out.println("No play");
+               break;
+           case 1 :
+               System.out.println("Hurray ! You got Ladder. ");
+               int bonusRoll = rand.nextInt(6)+1;
+               System.out.println("Dieroll is "+bonusRoll);
+               position1+=bonusRoll;
+               System.out.println(position1);
+               break;
+           default:
+               System.out.println("OOPS!!You attacked by snake.");
 
+               position1-=dieRoll;
+               System.out.println(position1);
+       }
     }
 }

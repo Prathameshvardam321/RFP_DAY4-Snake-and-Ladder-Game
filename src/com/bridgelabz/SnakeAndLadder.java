e9cc20312388ae4fcd94c8f1af67d33cfe29f5ed
Prathameshvardam321 <prathameshvardam321@gmail.com>
2022-09-30 13:06:34 +0530

    Added UC2.

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,7 +1,13 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
     public static void main(String[] args) {
-        int position1 = 0;      //Player1 position will start with 0.
+        Random rand = new Random();
+        int position1=0;
+        int dieRoll = rand.nextInt(6)+1;
+        System.out.println(dieRoll);
+
     }
 }

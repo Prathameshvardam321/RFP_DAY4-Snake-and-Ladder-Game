53c2c2a03955ba7ced54bae592c59196103850da
Prathameshvardam321 <prathameshvardam321@gmail.com>
2022-09-30 19:03:00 +0530

    Added UC6.

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -6,12 +6,19 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         Random rand = new Random();
         int position1 = 0;
+        int numRoll = 0;
         while (position1 < 100) {
             int dieRoll = rand.nextInt(6) + 1;
             System.out.println("Die roll is : " + dieRoll);
             position1 += dieRoll;
+            numRoll++;
+
+            System.out.println(numRoll+" times dice has been rolled.");
             if (position1>100){
                 position1-=dieRoll;
+                numRoll++;
+                System.out.println("Player position is : "+position1);
+                System.out.println(numRoll+" times dice has been rolled.");
             }
             System.out.println("Player position : " + position1);
             int option = rand.nextInt(3);
@@ -25,7 +32,9 @@ public class SnakeAndLadder {
                         int bonusRoll = rand.nextInt(6) + 1;
                         System.out.println("Die roll is " + bonusRoll);
                         position1 += bonusRoll;
+                        numRoll++;
                         System.out.println("Player position is : "+position1);
+                        System.out.println(numRoll+" times dice has been rolled.");
                         break;
                     default:
                         System.out.println("OOPS!!You attacked by snake.");

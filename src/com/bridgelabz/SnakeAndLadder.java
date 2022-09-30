341f511074b924f809a316703ce938712ee8d310
Prathameshvardam321 <prathameshvardam321@gmail.com>
2022-09-30 18:05:23 +0530

    Added UC4.

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -5,28 +5,37 @@ import java.util.Random;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         Random rand = new Random();
-        int position1=0;
-        int dieRoll = rand.nextInt(6)+1;
-        System.out.println("Dieroll is : "+dieRoll);
-        position1+=dieRoll;
-        System.out.println("Player 1 : "+position1);
-        int option = rand.nextInt(3);
-       switch (option){
-           case 0 :
-               System.out.println("No play");
-               break;
-           case 1 :
-               System.out.println("Hurray ! You got Ladder. ");
-               int bonusRoll = rand.nextInt(6)+1;
-               System.out.println("Dieroll is "+bonusRoll);
-               position1+=bonusRoll;
-               System.out.println(position1);
-               break;
-           default:
-               System.out.println("OOPS!!You attacked by snake.");
+        int position1 = 0;
+        while (position1 < 100) {
+            int dieRoll = rand.nextInt(6) + 1;
+            System.out.println("Dieroll is : " + dieRoll);
+            position1 += dieRoll;
+            if (position1>100){
+                position1-=dieRoll;
+            }
+            System.out.println("Player position : " + position1);
+            int option = rand.nextInt(3);
+            if ( position1>=10 && position1<=85 ) {
+                switch (option) {
+                    case 0:
+                        System.out.println("Condition : No play");
+                        break;
+                    case 1:
+                        System.out.println("Hurray ! You got Ladder. ");
+                        int bonusRoll = rand.nextInt(6) + 1;
+                        System.out.println("Dieroll is " + bonusRoll);
+                        position1 += bonusRoll;
+                        System.out.println("Player position is : "+position1);
+                        break;
+                    default:
+                        System.out.println("OOPS!!You attacked by snake.");
 
-               position1-=dieRoll;
-               System.out.println(position1);
-       }
+                        position1 -= dieRoll;
+                        System.out.println("Player position is : "+position1);
+
+                }
+            }
+
+        }
     }
 }

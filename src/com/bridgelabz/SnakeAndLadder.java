bfdef9980c4393340b65e3e0b7381dbfb050b824
Prathameshvardam321 <prathameshvardam321@gmail.com>
2022-09-30 12:59:02 +0530

    Added UC1.

---
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        int position1 = 0;      //Player1 position will start with 0.
+    }
+}
